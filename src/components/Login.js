256305bc0c6ecb6449ad0adb76435dc65ec80ba1
Jacqueline Michelle Paredes <jacquelinep_@hotmail.com>
2021-02-02 12:26:57 -0300

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -34,7 +34,7 @@ const Login = ({ history }) => {
   const { currentUser } = useContext(AuthContext);
 
   if (currentUser) {
-    return <Redirect to="/" />;
+    return <Redirect to="https://jaqomp.github.io/emprender" />;
   }
 
  makeStyles((theme) => ({
